c6140c12986bc143eabb5f9a0b25ae28ec95c7ed
Dranyl <Alfielynard23@gmail.com>
2026-07-22 22:14:03 +0800

    Fix favicon URL mixed content issue on Vercel by using relative path

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{ config('portfolio.personal.name') }} - Portfolio</title>
-    <link rel="icon" type="image/png" href="{{ asset('images/profile.png') }}">
+    <link rel="icon" type="image/png" href="/images/profile.png">
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
